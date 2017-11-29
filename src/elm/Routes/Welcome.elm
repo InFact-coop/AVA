eb170a0fa6a1d195eeae44f17d27f57f22907049
jen-harris <jennifer.louise.harris@gmail.com>
2017-11-29 17:58:18 +0000

    adds initial styling to welcome screen #9

--- src/elm/Routes/Welcome.elm
+++ src/elm/Routes/Welcome.elm
@@ -7,4 +7,15 @@ import Types exposing (..)
 
 welcome : Model -> Html Msg
 welcome model =
-    div [] [ text "This is welcome" ]
+    div [ class "pa4" ]
+        [ div [ class "pb3" ]
+            [ img [ class "br1", src "./assets/happywomen.jpg" ]
+                []
+            ]
+        , div [ class "bg-light-gray pa3" ]
+            [ div [ class "br1 pa3 bg-light-gray b tc dark-gray " ]
+                [ text "I am a survivor and I want to make a change." ]
+            , div [ class "br1 tc w3 h3 bg-white m0-auto" ]
+                []
+            ]
+        ]
