4f0429c8a71e41973e66495ccb99ca7d36a49f98
jen-harris <jennifer.louise.harris@gmail.com>
2017-11-30 11:47:05 +0000

    adds styling to thank you page relates #15

--- src/elm/Routes/Thanks.elm
+++ src/elm/Routes/Thanks.elm
@@ -7,4 +7,22 @@ import Types exposing (..)
 
 thanks : Model -> Html Msg
 thanks model =
-    div [] [ text "Thankszzz!" ]
+    div [ class "pa1" ]
+        [ div [ class "pa1 w-100 flex justify-center items-center" ]
+            [ div [ class "mw5 tc ma0-auto self-center" ]
+                [ h2 [ class "dark-gray b f2 tc mt2 mb2" ]
+                    [ text "Thank you for sharing " ]
+                , p [ class "tc fw1" ]
+                    [ text "We hope your message will inspire change for women across the country!"
+                    , p []
+                        []
+                    ]
+                ]
+            ]
+        , div [ class "flex flex-wrap justify-center" ]
+            [ div [ class "bg-light-teal white pa3 ma2 w-40 tc br1" ] [ text "something to choose here" ]
+            , div [ class "bg-light-teal white pa3 ma2 w-40 tc br1" ] [ text "something to choose here" ]
+            , div [ class "bg-light-teal white pa3 ma2 w-40 tc br1" ] [ text "something to choose here" ]
+            , div [ class "bg-light-teal white pa3 ma2 w-40 tc br1" ] [ text "something to choose here" ]
+            ]
+        ]
