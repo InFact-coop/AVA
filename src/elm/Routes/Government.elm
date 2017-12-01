508b92c14c94eda3d93178d1a52a9f91f6c9f65b
jen-harris <jennifer.louise.harris@gmail.com>
2017-12-01 15:52:12 +0000

    tweaks styling relates #29

--- src/elm/Routes/Government.elm
+++ src/elm/Routes/Government.elm
@@ -8,7 +8,7 @@ import Types exposing (..)
 government : Model -> Html Msg
 government model =
     div [ class "pa1" ]
-        [ div [ class "pa3 mt3 mb1 dark-gray b f4" ]
+        [ div [ class "pa2  dark-gray b f4" ]
             [ text "What message would you like to give to:" ]
         , div [ class "flex justify-center" ]
             [ input [ id "local", name "government", type_ "radio", value "Local Services", class "dn" ]
@@ -24,13 +24,12 @@ government model =
             [ textarea [ class "bg-light-gray ba b--mid-gray w-90", placeholder "Please tell us your message here" ]
                 []
             ]
-
-        , div [ class "pa3 mt3 mb1 dark-gray b f4" ]
+        , div [ class "pa1 mt4  dark-gray b f4" ]
             [ text "Are you happy if your journey or message is shared?" ]
         , div [ class "ml3 mt3 mb3" ]
             [ input [ id "yes", name "yesno", type_ "radio", value "yes", class "dn" ]
                 []
-            , label [ for "yes", class "bg-white pa2 pl4 pr4  pointer ba b--mid-gray mw4 br--left br1 tc" ]
+            , label [ for "yes", class "bg-white pa2 pl4 pr4 pointer ba b--mid-gray mw4 br--left br1 tc" ]
                 [ text "Yes " ]
             , input [ id "no", name "yesno", type_ "radio", value "no", class "dn" ]
                 []
